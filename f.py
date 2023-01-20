c76799398cbb5dd874fab9361403ed899b1853d4
Anova <119845896+Anova-12@users.noreply.github.com>
2023-01-20 19:41:07 +0545

    python basic

---
+++ f.py
@@ -0,0 +1,7 @@
+numbers = [6,2,2,6,2,2]
+for num_of_x in numbers:
+    output = ''
+    for ot in range(num_of_x):
+        output = output + 'a'
+    print(output)
+    
---
+++ nested.py
@@ -0,0 +1,3 @@
+for a in range(7):
+    for b in range(5):
+        print(a , b)
