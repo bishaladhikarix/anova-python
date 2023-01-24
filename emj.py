83a8ad039dab802d437872ba1bdfe56798ce0d15
Anova <119845896+Anova-12@users.noreply.github.com>
2023-01-24 16:12:53 +0545

    function

--- emj.py
+++ emj.py
@@ -1,10 +1,16 @@
 message = input("> ")
-word = message.split()
-emoji = {
+
+
+def first_function(message):
+    word = message.split()
+    emoji ={
     ":)":"😁",
     ":(":"😢"
-}
-output = ""
-for each in word:
-    output += emoji.get(each, each)
-print(output)
+    }
+    output = ""
+    for each in word:
+        output += emoji.get(each, each)
+    return output
+
+resut = first_function(message)
+print(resut)
